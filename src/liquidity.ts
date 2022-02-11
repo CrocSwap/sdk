3f34d08620385dc9bca4799bcde787f5b1bb580d
Ben Wolski <benwolski@gmail.com>
2022-02-11 10:41:45 -0500

    add logic to only attach ETH to ambient mint when necessary

--- src/liquidity.ts
+++ src/liquidity.ts
@@ -3,7 +3,6 @@ import { MAX_LIQ, contractAddresses } from "./constants";
 import { bigNumToFloat, floatToBigNum, encodeCrocPrice } from "./utils";
 import { CROC_ABI } from "./abis";
 
-
 type Address = string;
 type PoolType = number;
 
@@ -15,7 +14,7 @@ type PoolType = number;
  * @param qty The quantity (in non-display wei) of base token to convert
  * @return The amount of virtual liquidity (in sqrt(X*Y)) supported by this base token quantity. */
 export function liquidityForBaseQty(price: number, qty: BigNumber): BigNumber {
-  return floatToBigNum(Math.floor(bigNumToFloat(qty) * Math.sqrt(price)))
+  return floatToBigNum(Math.floor(bigNumToFloat(qty) * Math.sqrt(price)));
 }
 
 /* Converts a fixed quote token collateral amount to pool liquidity units. This conversion only applies
@@ -30,60 +29,165 @@ export function liquidityForQuoteQty(price: number, qty: BigNumber): BigNumber {
 }
 
 export class WarmPathEncoder {
+  constructor(base: Address, quote: Address, poolIdx: PoolType) {
+    this.base = base;
+    this.quote = quote;
+    this.poolIdx = poolIdx;
+    this.abiCoder = new ethers.utils.AbiCoder();
+  }
 
-    constructor (base: Address, quote: Address, poolIdx: PoolType) {
-        this.base = base
-        this.quote = quote
-        this.poolIdx = poolIdx
-        this.abiCoder = new ethers.utils.AbiCoder()
-    }
+  private base: Address;
+  private quote: Address;
+  private poolIdx: PoolType;
+  private abiCoder: ethers.utils.AbiCoder;
 
-    private base: Address
-    private quote: Address
-    private poolIdx: PoolType;
-    private abiCoder: ethers.utils.AbiCoder;
+  private readonly MINT_CONCENTRATED: number = 1;
+  private readonly BURN_CONCENTRATED: number = 2;
+  private readonly MINT_AMBIENT: number = 3;
+  private readonly BURN_AMBIENT: number = 4;
 
-    private readonly MINT_CONCENTRATED: number = 1;
-    private readonly BURN_CONCENTRATED: number = 2;
-    private readonly MINT_AMBIENT: number = 3;
-    private readonly BURN_AMBIENT: number = 4;
+  encodeMintConc(
+    lowerTick: number,
+    upperTick: number,
+    liq: BigNumber,
+    limitLow: number,
+    limitHigh: number,
+    useSurplus: boolean
+  ) {
+    return this.encodeWarmPath(
+      this.MINT_CONCENTRATED,
+      lowerTick,
+      upperTick,
+      liq,
+      limitLow,
+      limitHigh,
+      useSurplus
+    );
+  }
 
+  encodeBurnConc(
+    lowerTick: number,
+    upperTick: number,
+    liq: BigNumber,
+    limitLow: number,
+    limitHigh: number,
+    useSurplus: boolean
+  ) {
+    return this.encodeWarmPath(
+      this.BURN_CONCENTRATED,
+      lowerTick,
+      upperTick,
+      liq,
+      limitLow,
+      limitHigh,
+      useSurplus
+    );
+  }
 
-    encodeMintConc (lowerTick: number, upperTick: number, liq: BigNumber, limitLow: number, limitHigh: number,
-        useSurplus: boolean) {
-        return this.encodeWarmPath(this.MINT_CONCENTRATED, lowerTick, upperTick, liq, 
-            limitLow, limitHigh, useSurplus)
-    }
+  encodeBurnConcAll(
+    lowerTick: number,
+    upperTick: number,
+    limitLow: number,
+    limitHigh: number,
+    useSurplus: boolean
+  ) {
+    return this.encodeWarmPath(
+      this.BURN_CONCENTRATED,
+      lowerTick,
+      upperTick,
+      MAX_LIQ,
+      limitLow,
+      limitHigh,
+      useSurplus
+    );
+  }
 
-    encodeBurnConc (lowerTick: number, upperTick: number, liq: BigNumber, limitLow: number, limitHigh: number,
-        useSurplus: boolean) {
-        return this.encodeWarmPath(this.BURN_CONCENTRATED, lowerTick, upperTick, liq, limitLow, limitHigh, useSurplus)
-    }
+  encodeMintAmbient(
+    liq: BigNumber,
+    limitLow: number,
+    limitHigh: number,
+    useSurplus: boolean
+  ) {
+    return this.encodeWarmPath(
+      this.MINT_AMBIENT,
+      0,
+      0,
+      liq,
+      limitLow,
+      limitHigh,
+      useSurplus
+    );
+  }
 
-    encodeBurnConcAll (lowerTick: number, upperTick: number, limitLow: number, limitHigh: number,
-        useSurplus: boolean) {
-        return this.encodeWarmPath(this.BURN_CONCENTRATED, lowerTick, upperTick, MAX_LIQ, limitLow, limitHigh, useSurplus)
-    }
+  encodeBurnAmbient(
+    liq: BigNumber,
+    limitLow: number,
+    limitHigh: number,
+    useSurplus: boolean
+  ) {
+    return this.encodeWarmPath(
+      this.BURN_AMBIENT,
+      0,
+      0,
+      liq,
+      limitLow,
+      limitHigh,
+      useSurplus
+    );
+  }
 
-    encodeMintAmbient (liq: BigNumber, limitLow: number, limitHigh: number, useSurplus: boolean) {
-        return this.encodeWarmPath(this.MINT_AMBIENT, 0, 0, liq, limitLow, limitHigh, useSurplus)
-    }
+  encodeBurnAmbientAll(
+    limitLow: number,
+    limitHigh: number,
+    useSurplus: boolean
+  ) {
+    return this.encodeWarmPath(
+      this.BURN_AMBIENT,
+      0,
+      0,
+      MAX_LIQ,
+      limitLow,
+      limitHigh,
+      useSurplus
+    );
+  }
 
-    encodeBurnAmbient (liq: BigNumber, limitLow: number, limitHigh: number, useSurplus: boolean) {
-        return this.encodeWarmPath(this.BURN_AMBIENT, 0, 0, liq, limitLow, limitHigh, useSurplus)
-    }
-
-    encodeBurnAmbientAll (limitLow: number, limitHigh: number, useSurplus: boolean) {
-        return this.encodeWarmPath(this.BURN_AMBIENT, 0, 0, MAX_LIQ, limitLow, limitHigh, useSurplus)
-    }
-
-    private encodeWarmPath (callCode: number, lowerTick: number, upperTick: number, liq: BigNumber, 
-        limitLow: number, limitHigh: number, useSurplus: boolean): string {
-        return this.abiCoder.encode(
-            [ "uint8", "address", "address", "uint24", "int24", "int24", "uint128", "uint128", "uint128", "bool" ], 
-            [ callCode, this.base, this.quote, this.poolIdx, lowerTick, upperTick, liq, 
-                encodeCrocPrice(limitLow), encodeCrocPrice(limitHigh), useSurplus  ]);
-    }
+  private encodeWarmPath(
+    callCode: number,
+    lowerTick: number,
+    upperTick: number,
+    liq: BigNumber,
+    limitLow: number,
+    limitHigh: number,
+    useSurplus: boolean
+  ): string {
+    return this.abiCoder.encode(
+      [
+        "uint8",
+        "address",
+        "address",
+        "uint24",
+        "int24",
+        "int24",
+        "uint128",
+        "uint128",
+        "uint128",
+        "bool",
+      ],
+      [
+        callCode,
+        this.base,
+        this.quote,
+        this.poolIdx,
+        lowerTick,
+        upperTick,
+        liq,
+        encodeCrocPrice(limitLow),
+        encodeCrocPrice(limitHigh),
+        useSurplus,
+      ]
+    );
+  }
 }
 
 export async function sendAmbientMint(
@@ -111,9 +215,19 @@ export async function sendAmbientMint(
     limitHigh,
     false
   );
-  const tx = await crocContract.tradeWarm(args, {
-    value: BigNumber.from(10).pow(18),
-    gasLimit: 1000000,
-  });
+
+  let tx;
+  // if baseToken = ETH
+  if (baseTokenAddress === contractAddresses.ZERO_ADDR) {
+    tx = await crocContract.tradeWarm(args, {
+      value: BigNumber.from(10).pow(18),
+      gasLimit: 1000000,
+    });
+  } else {
+    tx = await crocContract.tradeWarm(args, {
+      gasLimit: 1000000,
+    });
+  }
+
   return tx;
 }
