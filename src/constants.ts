3bc0efe64f4412315c68c10ff637570610e08740
Ben Wolski <benwolski@gmail.com>
2022-02-18 13:42:34 -0500

    update MAX_LIQ to .sub(1)

--- src/constants.ts
+++ src/constants.ts
@@ -9,17 +9,17 @@ export const contractAddresses = {
 export const MIN_TICK = -665454;
 export const MAX_TICK = 831818;
 
-export const MAX_SQRT_PRICE: BigNumber =
-  BigNumber.from("21267430153580247136652501917186561138").sub(1);
+export const MAX_SQRT_PRICE: BigNumber = BigNumber.from(
+  "21267430153580247136652501917186561138"
+).sub(1);
 
-export const MIN_SQRT_PRICE: BigNumber =
-  BigNumber.from("65538").sub(1);
+export const MIN_SQRT_PRICE: BigNumber = BigNumber.from("65538").sub(1);
 
-export const MAX_LIQ = BigNumber.from(2).pow(128)
+export const MAX_LIQ = BigNumber.from(2).pow(128).sub(1);
 
 export const NODE_URL =
-    "https://speedy-nodes-nyc.moralis.io/015fffb61180886c9708499e/eth/ropsten";
+  "https://speedy-nodes-nyc.moralis.io/015fffb61180886c9708499e/eth/ropsten";
 
-export const POOL_PRIMARY = 35000
+export const POOL_PRIMARY = 35000;
 
-export const GRID_SIZE_DFLT = 30
+export const GRID_SIZE_DFLT = 30;
