9e13b791064a5d54e28ca0528d5a45473061ab27
Douglas R Colkitt <douglas.colkitt@gmail.com>
2022-07-16 03:16:03 +0000

    Update addresses and RPC node for migration to Goerli testnet

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,8 @@ import { BigNumber } from "ethers";
 
 export const contractAddresses = {
   ZERO_ADDR: "0x0000000000000000000000000000000000000000",
-  CROC_SWAP_ADDR: "0x5d42d6046927dee12b9b4a235be0cecd55d0e0fb",
-  QUERY_ADDR: "0x3a6e9cff691a473d4d0742e1dfc8ea263a99f6d0",
+  CROC_SWAP_ADDR: "0xfafcd1f5530827e7398b6d3c509f450b1b24a209",
+  QUERY_ADDR: "0x9ea4b2f9b1572ed3ac46b402d9ba9153821033c6",
 };
 
 export const MIN_TICK = -665454;
@@ -18,7 +18,7 @@ export const MIN_SQRT_PRICE: BigNumber = BigNumber.from("65538").sub(1);
 export const MAX_LIQ = BigNumber.from(2).pow(128).sub(1);
 
 export const NODE_URL =
-  "https://speedy-nodes-nyc.moralis.io/015fffb61180886c9708499e/eth/kovan";
+  "https://speedy-nodes-nyc.moralis.io/015fffb61180886c9708499e/eth/goerli";
 
 export const POOL_PRIMARY = 36000;
 
