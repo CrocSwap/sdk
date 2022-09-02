657491f5bde18ee1fb7396f9dfdc1774730428be
Douglas Colkitt <douglas.colkitt@gmail.com>
2022-09-02 00:15:57 -0400

    Add burn range example to demo.ts

--- src/examples/demo.ts
+++ src/examples/demo.ts
@@ -34,6 +34,7 @@ async function demo() {
     //ko = new CrocKnockoutHandle(AddressZero, DAI, 0.01, -78464-1024, croc.context)
     await (await ko.mint()).wait()*/
 
+    //let tx = croc.poolEth(DAI).burnAmbientLiq(BigNumber.from(10).pow(7), [0.0001, 0.001])
     //let tx = croc.poolEth(DAI).mintRangeBase(0.0001, [-640000, 640000], [0.0001, 0.001])
     //let tx = croc.poolEth(DAI).mintRangeBase(1.0, [-100000, 0], [0.0001, 0.001])
     let tx = croc.poolEth(DAI).burnRangeLiq(BigNumber.from(10).pow(7), [-640000, 640000], [0.0001, 0.001])
