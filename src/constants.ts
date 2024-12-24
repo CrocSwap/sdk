a601ef87d209a388c566361ed53586d055db55d0
Ben Wolski <570819+benwolski@users.noreply.github.com>
2024-12-24 09:23:22 -0500

    remove infura dependency

--- src/constants.ts
+++ src/constants.ts
@@ -11,7 +11,6 @@ export const MAX_LIQ = BigInt(2) ** BigInt(128) - BigInt(1);
 
 export interface ChainSpec {
   nodeUrl: string;
-  wsUrl?: string,
   poolIndex: number;
   addrs: {
     dex: ChainAddress;
@@ -41,55 +40,8 @@ const SCROLL_LOGO =
 const BLAST_LOGO =
  "https://assets-global.website-files.com/65a6baa1a3f8ed336f415cb4/65a6cc95aae1066cf96d497d_Logo%20Black%20on%20Yellow%20Background%402x-p-500.png"
 
-const DFLT_SDK_INFURA_KEY = '4741d1713bff4013bc3075ed6e7ce091'
-
-const GOERLI_CHAIN: ChainSpec = {
-  nodeUrl: "https://goerli.infura.io/v3/" + DFLT_SDK_INFURA_KEY,
-  wsUrl: "wss://goerli.infura.io/ws/v3/" + DFLT_SDK_INFURA_KEY,
-  addrs: {
-    dex: "0xfafcd1f5530827e7398b6d3c509f450b1b24a209",
-    query: "0xc9900777baa5EE94Cd2C6509fb09278A1A46b7e8",
-    impact: "0x142BE02F2A3A27ecD6e2f18a43c2C234F372C831"
-  },
-  poolIndex: 36000,
-  isTestNet: true,
-  chainId: "0x5",
-  gridSize: 64,
-  proxyPaths: {
-    cold: 0,
-    long: 4,
-    liq: 2
-  },
-  blockExplorer: "https://goerli.etherscan.io/",
-  displayName: "Görli",
-  logoUrl: ETHEREUM_LOGO,
-};
-
-
-const ARB_GOERLI_CHAIN: ChainSpec = {
-  nodeUrl: "https://goerli-rollup.arbitrum.io/rpc",
-  addrs: {
-    dex: "0x5D42d6046927DEE12b9b4a235be0ceCd55D0E0fb",
-    query: "0x3A6E9cff691a473D4D0742E1dFc8Ea263a99F6d0",
-    impact: "0xf19D3dcdF82af0d40Cb3b4AaE4D266c638A3E454"
-  },
-  poolIndex: 36000,
-  isTestNet: true,
-  chainId: "0x66eed",
-  gridSize: 16,
-  proxyPaths: {
-    cold: 3,
-    long: 4,
-    liq: 2
-  },
-  blockExplorer: "https://goerli.arbiscan.io/",
-  displayName: "Arbitrum Görli",
-  logoUrl: ETHEREUM_LOGO,
-};
-
 const MAINNET_CHAIN: ChainSpec = {
-  nodeUrl: "https://mainnet.infura.io/v3/" + DFLT_SDK_INFURA_KEY,
-  wsUrl: "wss://mainnet.infura.io/ws/v3/" + DFLT_SDK_INFURA_KEY,
+  nodeUrl: "https://ethereum-rpc.publicnode.com" ,
   addrs: {
     dex: "0xAaAaAAAaA24eEeb8d57D431224f73832bC34f688",
     query: "0xc2e1f740E11294C64adE66f69a1271C5B32004c8",
@@ -113,8 +65,7 @@ const MAINNET_CHAIN: ChainSpec = {
 };
 
 const SEPOLIA_CHAIN: ChainSpec = {
-  nodeUrl: "https://sepolia.infura.io/v3/" + DFLT_SDK_INFURA_KEY,
-  wsUrl: "wss://sepolia.infura.io/ws/v3/" + DFLT_SDK_INFURA_KEY,
+  nodeUrl: "https://ethereum-sepolia-rpc.publicnode.com",
   addrs: {
     dex: "0xFb8A46E7963E6397DBB4B2E1c0B3f0464fb5BDFF",
     query: "0xDB182F4687708D0F5798c77b4d02ad3425f4B672",
@@ -362,17 +313,8 @@ const SCROLL_CHAIN: ChainSpec = {
 };
 
 
-const LOCAL_FORK_CHAIN: ChainSpec = Object.assign({}, GOERLI_CHAIN, {
-  nodeUrl: "http://127.0.0.1:8545",
-  chainId: "0x7a69",
-  displayName: "Local Fork"
-});
-
 export const CHAIN_SPECS: { [chainId: string]: ChainSpec } = {
   "0x1": MAINNET_CHAIN,
-  "0x5": GOERLI_CHAIN,
-  "0x7a69": LOCAL_FORK_CHAIN,
-  "0x66eed": ARB_GOERLI_CHAIN,
   "0xaa36a7": SEPOLIA_CHAIN,
   "0x8274f": SCROLL_SEPOLIA_CHAIN,
   "0x82750": SCROLL_CHAIN,
@@ -383,11 +325,7 @@ export const CHAIN_SPECS: { [chainId: string]: ChainSpec } = {
   "0x783": SWELL_CHAIN,
   "0x14a34": BASE_SEPOLIA_CHAIN,
   "0x18231": PLUME_CHAIN,
-  "goerli": GOERLI_CHAIN,
   "sepolia": SEPOLIA_CHAIN,
-  "arbtest": ARB_GOERLI_CHAIN,
-  "arbgoerli": ARB_GOERLI_CHAIN,
-  "local": LOCAL_FORK_CHAIN,
   "ethereum": MAINNET_CHAIN,
   "mainnet": MAINNET_CHAIN,
   "scrolltest": SCROLL_SEPOLIA_CHAIN,
